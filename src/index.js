4c94801916ac34c3b50045a64ecec1696ec5c34e
ru3ant <61232689+ru3ant@users.noreply.github.com>
2020-05-12 02:03:35 +0300

    React Componets

--- src/index.js
+++ src/index.js
@@ -1,16 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const el = (
-  <div>
-    <h1>ToDo App</h1>
-    <input placeholder="search" />
+const TodoList = () => {
+  return (
     <ul>
       <li>Important Do</li>
       <li>More important</li>
     </ul>
-  </div>
-);
+  );
+};
+
+const SearchPanel = () => {
+  return <input placeholder="search" />;
+};
+
+const AppHeader = () => {
+  return <h1>ToDo App</h1>;
+};
+
+const App = () => {
+  return (
+    <div>
+      <AppHeader />
+      <SearchPanel />
+      <TodoList />
+    </div>
+  );
+};
 
 const divRoot = document.getElementById("root");
-ReactDOM.render(el, divRoot);
+ReactDOM.render(<App />, divRoot);
